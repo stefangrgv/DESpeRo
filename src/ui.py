119cf5c2991f91c59c989f1b4cfb8022c32018bf
stefangrgv <stefan.grgv@gmail.com>
2025-05-11 12:35:27 +0300

    UI: improve styles

--- src/ui.py
+++ src/ui.py
@@ -7,12 +7,14 @@ from tkinter.filedialog import askdirectory
 from src.drs_run import DRS_Run
 
 NO_DIR_STRING = "No selected directory"
+BACKGROUND_COLORS = {"MAIN": "#ebebeb", "BUTTON": "#e0e0e0"}
 
 
 class UI:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("DESpeRo")
+        self.root.config(bg=BACKGROUND_COLORS["MAIN"])
 
     def render_welcome_screen(self) -> None:
         self.observations_dir = StringVar()
@@ -51,111 +53,158 @@ class UI:
             var.trace_add("write", lambda *args: self._update_start_button_state())
 
         # frames
-        container = tk.Frame(self.root)
-        container.pack(expand=True)
+        self.container_frame = tk.Frame(self.root, bg=BACKGROUND_COLORS["MAIN"])
+        self.container_frame.pack(expand=True, padx=30, pady=10)
 
-        dir_frame = tk.Frame(container)
-        dir_frame.pack(pady=10, anchor="w")
+        dir_frame = tk.Frame(self.container_frame, bg=BACKGROUND_COLORS["MAIN"])
+        dir_frame.pack(anchor="w")
 
-        config_frame = tk.Frame(container)
-        config_frame.pack(pady=10, anchor="w")
+        self.config_frame = tk.Frame(self.container_frame, bg=BACKGROUND_COLORS["MAIN"])
+        self.config_frame.pack(pady=10, anchor="w")
 
-        output_frame = tk.Frame(container)
-        output_frame.pack(pady=10, anchor="w")
+        self.output_frame = tk.Frame(self.container_frame, bg=BACKGROUND_COLORS["MAIN"])
+        self.output_frame.pack(pady=10, anchor="w")
 
-        start_frame = tk.Frame(container)
-        start_frame.pack(pady=10, anchor="w", fill="x", expand=True)
+        self.start_frame = tk.Frame(self.container_frame, bg=BACKGROUND_COLORS["MAIN"])
+        self.start_frame.pack(pady=10, anchor="w", fill="x", expand=True)
 
         # elements
-        directory_label = tk.Label(dir_frame, textvariable=self.observations_dir_truncated)
+        directory_label = tk.Label(
+            dir_frame, textvariable=self.observations_dir_truncated, bg=BACKGROUND_COLORS["MAIN"]
+        )
         directory_label.pack(side=tk.LEFT, padx=5)
 
-        select_button = tk.Button(dir_frame, text="Select", command=self._select_directory)
-        select_button.pack(side=tk.RIGHT, padx=5)
+        self.select_button = tk.Button(
+            dir_frame, text="Select", command=self._select_directory, bg=BACKGROUND_COLORS["BUTTON"]
+        )
+        self.select_button.pack(side=tk.RIGHT, padx=5)
 
-        self.cosmic_checkbox = tk.Checkbutton(config_frame, text="Remove cosmics", variable=self.cosmic)
+        self.cosmic_checkbox = tk.Checkbutton(
+            self.config_frame,
+            text="Remove cosmics",
+            variable=self.cosmic,
+            bg=BACKGROUND_COLORS["MAIN"],
+            highlightthickness=0,
+        )
         self.cosmic_checkbox.pack(pady=5, anchor="w")
 
-        self.bias_checkbox = tk.Checkbutton(config_frame, text="Correct for bias", variable=self.bias)
+        self.bias_checkbox = tk.Checkbutton(
+            self.config_frame,
+            text="Correct for bias",
+            variable=self.bias,
+            bg=BACKGROUND_COLORS["MAIN"],
+            highlightthickness=0,
+        )
         self.bias_checkbox.pack(pady=5, anchor="w")
         self.bias_checkbox.config(state=tk.DISABLED)
 
-        self.flat_checkbox = tk.Checkbutton(config_frame, text="Correct for flat", variable=self.flat)
+        self.flat_checkbox = tk.Checkbutton(
+            self.config_frame,
+            text="Correct for flat",
+            variable=self.flat,
+            bg=BACKGROUND_COLORS["MAIN"],
+            highlightthickness=0,
+        )
         self.flat_checkbox.pack(pady=5, anchor="w")
         self.flat_checkbox.config(state=tk.DISABLED)
 
-        self.vhelio_checkbox = tk.Checkbutton(config_frame, text="Correct for VHELIO", variable=self.vhelio)
+        self.vhelio_checkbox = tk.Checkbutton(
+            self.config_frame,
+            text="Correct for VHELIO",
+            variable=self.vhelio,
+            bg=BACKGROUND_COLORS["MAIN"],
+            highlightthickness=0,
+        )
         self.vhelio_checkbox.pack(pady=5, anchor="w")
         self.vhelio_checkbox.config(state=tk.DISABLED)
 
         output_text = tk.StringVar()
         output_text.set("Output format")
-        output_label = tk.Label(output_frame, textvariable=output_text)
+        output_label = tk.Label(self.output_frame, textvariable=output_text, bg=BACKGROUND_COLORS["MAIN"])
         output_label.pack(pady=5, anchor="w")
 
-        self.fits_2d_checkbox = tk.Checkbutton(output_frame, text="2D FITS", variable=self.fits_2d)
+        self.fits_2d_checkbox = tk.Checkbutton(
+            self.output_frame, text="2D FITS", variable=self.fits_2d, bg=BACKGROUND_COLORS["MAIN"], highlightthickness=0
+        )
         self.fits_2d_checkbox.pack(pady=5, anchor="w")
         self.fits_2d_checkbox.config(state=tk.DISABLED)
 
         self.fits_2d_norm_checkbox = tk.Checkbutton(
-            output_frame, text="2D FITS (normalized)", variable=self.fits_2d_norm
+            self.output_frame,
+            text="2D FITS (normalized)",
+            variable=self.fits_2d_norm,
+            bg=BACKGROUND_COLORS["MAIN"],
+            highlightthickness=0,
         )
         self.fits_2d_norm_checkbox.pack(pady=5, anchor="w")
         self.fits_2d_norm_checkbox.config(state=tk.DISABLED)
 
-        self.ascii_2d_checkbox = tk.Checkbutton(output_frame, text="2D ASCII", variable=self.ascii_2d)
+        self.ascii_2d_checkbox = tk.Checkbutton(
+            self.output_frame,
+            text="2D ASCII",
+            variable=self.ascii_2d,
+            bg=BACKGROUND_COLORS["MAIN"],
+            highlightthickness=0,
+        )
         self.ascii_2d_checkbox.pack(pady=5, anchor="w")
         self.ascii_2d_checkbox.config(state=tk.DISABLED)
 
         self.ascii_2d_norm_checkbox = tk.Checkbutton(
-            output_frame, text="2D ASCII (normalized)", variable=self.ascii_2d_norm
+            self.output_frame,
+            text="2D ASCII (normalized)",
+            variable=self.ascii_2d_norm,
+            bg=BACKGROUND_COLORS["MAIN"],
+            highlightthickness=0,
         )
         self.ascii_2d_norm_checkbox.pack(pady=5, anchor="w")
         self.ascii_2d_norm_checkbox.config(state=tk.DISABLED)
 
         self.ascii_1d_norm_checkbox = tk.Checkbutton(
-            output_frame, text="1D ASCII (normalized)", variable=self.ascii_1d_norm
+            self.output_frame,
+            text="1D ASCII (normalized)",
+            variable=self.ascii_1d_norm,
+            bg=BACKGROUND_COLORS["MAIN"],
+            highlightthickness=0,
         )
         self.ascii_1d_norm_checkbox.pack(pady=5, anchor="w")
         self.ascii_1d_norm_checkbox.config(state=tk.DISABLED)
 
-        self.start_button = tk.Button(start_frame, text="GO", command=self._go)
+        self.start_button = tk.Button(self.start_frame, text="GO", command=self._go, bg=BACKGROUND_COLORS["BUTTON"])
         self.start_button.pack(fill="x", expand=True)
         self.start_button.config(state=tk.DISABLED)
 
         self._set_settings(state=False)
-        self.root.minsize(500, 300)
         self.root.mainloop()
 
     def set_status(self, name: str, finished: bool) -> None:
-        color = "green" if finished else "black"
+        color = "#147800" if finished else "black"
         label = None
         for l in self.steps_labels:
             if l["name"] == name:
                 label = l["label"]
                 break
-        if label is not None:
-            label.config(fg=color)
+        if label is None:
+            return
+        label.config(fg=color)
 
     def render_working_screen(self):
-        for widget in self.root.winfo_children():
-            widget.destroy()
+        self.config_frame.destroy()
+        self.output_frame.destroy()
+        self.start_frame.destroy()
+        self.select_button.destroy()
 
-        # frames
-        container = tk.Frame(self.root)
-        container.pack(fill="both", expand=True)
-
-        dir_frame = tk.Frame(container)
-        dir_frame.pack(anchor="w", fill="both", expand=True)
-
-        self.status_frame = tk.Frame(container)
-        self.status_frame.pack(anchor="w", fill="both", expand=True)
-
-        # elements
-        directory_label = tk.Label(dir_frame, textvariable=self.observations_dir_truncated)
-        directory_label.pack(expand=True)
+        self.status_frame = tk.Frame(self.container_frame, bg=BACKGROUND_COLORS["MAIN"])
+        self.status_frame.pack(anchor="w", fill="x", pady=10)
 
         self._init_steps_labels()
+
+    def _create_status_label(self, text: str):
+        return tk.Label(
+            master=self.status_frame,
+            fg="#949292",
+            bg=BACKGROUND_COLORS["MAIN"],
+            textvariable=tk.StringVar(master=self.status_frame, value=text),
+        )
 
     def _init_steps_labels(self):
         self.steps_labels = []
@@ -164,100 +213,64 @@ class UI:
             self.steps_labels.append(
                 {
                     "name": "cosmics",
-                    "label": tk.Label(
-                        master=self.status_frame,
-                        fg="gray",
-                        textvariable=tk.StringVar(master=self.status_frame, value="Clean cosmics"),
-                    ),
+                    "label": self._create_status_label("Clean cosmics"),
                 }
             )
         if self.bias.get():
             self.steps_labels.append(
                 {
                     "name": "bias",
-                    "label": tk.Label(
-                        master=self.status_frame,
-                        fg="gray",
-                        textvariable=tk.StringVar(master=self.status_frame, value="Correct for bias"),
-                    ),
+                    "label": self._create_status_label("Correct for bias"),
                 }
             )
         if self.flat.get():
             self.steps_labels.append(
                 {
                     "name": "flat",
-                    "label": tk.Label(
-                        master=self.status_frame,
-                        fg="gray",
-                        textvariable=tk.StringVar(master=self.status_frame, value="Correct for flat"),
-                    ),
+                    "label": self._create_status_label("Correct for flat"),
                 }
             )
         self.steps_labels.append(
             {
                 "name": "orders",
-                "label": tk.Label(
-                    master=self.status_frame,
-                    fg="gray",
-                    textvariable=tk.StringVar(master=self.status_frame, value="Trace echelle orders"),
-                ),
+                "label": self._create_status_label("Trace echelle orders"),
             }
         )
         self.steps_labels.append(
             {
                 "name": "spectra",
-                "label": tk.Label(
-                    master=self.status_frame,
-                    fg="gray",
-                    textvariable=tk.StringVar(master=self.status_frame, value="Extract spectra"),
-                ),
+                "label": self._create_status_label("Extract spectra"),
             }
         )
         self.steps_labels.append(
             {
                 "name": "wavelength",
-                "label": tk.Label(
-                    master=self.status_frame,
-                    fg="gray",
-                    textvariable=tk.StringVar(master=self.status_frame, value="Calibrate for wavelength"),
-                ),
+                "label": self._create_status_label("Calibrate for wavelength"),
             }
         )
         if self.fits_2d_norm.get() or self.ascii_2d_norm.get() or self.ascii_1d_norm.get():
             self.steps_labels.append(
                 {
                     "name": "normalize",
-                    "label": tk.Label(
-                        master=self.status_frame,
-                        fg="gray",
-                        textvariable=tk.StringVar(master=self.status_frame, value="Normalize spectra"),
-                    ),
+                    "label": self._create_status_label("Normalize spectra"),
                 }
             )
         if self.ascii_1d_norm.get():
             self.steps_labels.append(
                 {
                     "name": "stitch",
-                    "label": tk.Label(
-                        master=self.status_frame,
-                        fg="gray",
-                        textvariable=tk.StringVar(master=self.status_frame, value="Stitch spectra"),
-                    ),
+                    "label": self._create_status_label("Stitch spectra"),
                 }
             )
         self.steps_labels.append(
             {
                 "name": "save",
-                "label": tk.Label(
-                    master=self.status_frame,
-                    fg="gray",
-                    textvariable=tk.StringVar(master=self.status_frame, value="Save files"),
-                ),
+                "label": self._create_status_label("Save files"),
             }
         )
 
         for steps_label in self.steps_labels:
-            steps_label["label"].pack(expand=True)
+            steps_label["label"].pack(anchor="w")
 
     def _update_start_button_state(self):
         if any(var.get() for var in self.output_vars):
