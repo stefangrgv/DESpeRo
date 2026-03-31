a44738f2daeda350ec18b7d452739ea6a4679666
stefangrgv <stefan.grgv@gmail.com>
2026-03-31 21:59:12 +0300

    Fix problem with stitching 1D spectra

--- src/despero/normalize.py
+++ src/despero/normalize.py
@@ -31,26 +31,30 @@ def stitch_oned(stellar: Any) -> None:
     intensity = []  # to keep track of the SNR
     n_intensity = []
     for order in reversed_orders:
-        if len(wavelength) == 0 or order.wavelength[0] > wavelength[-1]:
-            # no overlap
-            wavelength = [*wavelength, *list(order.wavelength)]
-            intensity = [*intensity, *list(order.intensity)]
-            n_intensity = [*n_intensity, *list(order.normalized_intensity)]
-        else:
-            overlap_prev_ind = np.where(wavelength >= order.wavelength[0])[0]
-            overlap_next_ind = np.where(order.wavelength <= wavelength[-1])[0]
-            overlap_prev_signal = [intensity[i] for i in overlap_prev_ind]
-            overlap_next_signal = [order.intensity[i] for i in overlap_next_ind]
-            if np.mean(overlap_prev_signal) > np.mean(overlap_next_signal):
-                # previous (bluer) order has more signal
-                wavelength = [*wavelength, *list(order.wavelength[overlap_next_ind[-1] :])]
-                intensity = [*intensity, *list(order.intensity[overlap_next_ind[-1] :])]
-                n_intensity = [*n_intensity, *list(order.normalized_intensity[overlap_next_ind[-1] :])]
+        try:
+            if len(wavelength) == 0 or order.wavelength[0] > wavelength[-1]:
+                # no overlap
+                wavelength = [*wavelength, *list(order.wavelength)]
+                intensity = [*intensity, *list(order.intensity)]
+                n_intensity = [*n_intensity, *list(order.normalized_intensity)]
             else:
-                keep_ind = [i for i in range(len(wavelength)) if i not in overlap_prev_ind]
-                wavelength = [*[wavelength[i] for i in keep_ind], *list(order.wavelength)]
-                intensity = [*[intensity[i] for i in keep_ind], *list(order.intensity)]
-                n_intensity = [*[n_intensity[i] for i in keep_ind], *list(order.normalized_intensity)]
+                overlap_prev_ind = np.where(wavelength >= order.wavelength[0])[0]
+                overlap_next_ind = np.where(order.wavelength <= wavelength[-1])[0]
+                overlap_prev_signal = [intensity[i] for i in overlap_prev_ind]
+                overlap_next_signal = [order.intensity[i] for i in overlap_next_ind]
+                if np.mean(overlap_prev_signal) > np.mean(overlap_next_signal):
+                    # previous (bluer) order has more signal
+                    wavelength = [*wavelength, *list(order.wavelength[overlap_next_ind[-1] :])]
+                    intensity = [*intensity, *list(order.intensity[overlap_next_ind[-1] :])]
+                    n_intensity = [*n_intensity, *list(order.normalized_intensity[overlap_next_ind[-1] :])]
+                else:
+                    keep_ind = [i for i in range(len(wavelength)) if i not in overlap_prev_ind]
+                    wavelength = [*[wavelength[i] for i in keep_ind], *list(order.wavelength)]
+                    intensity = [*[intensity[i] for i in keep_ind], *list(order.intensity)]
+                    n_intensity = [*[n_intensity[i] for i in keep_ind], *list(order.normalized_intensity)]
+        except IndexError as exc:
+            # current order has no wavelength solution, skip it
+            continue
 
     stellar.oned_wavelength = np.asarray(wavelength)
     stellar.oned_intensity = np.asarray(n_intensity)
